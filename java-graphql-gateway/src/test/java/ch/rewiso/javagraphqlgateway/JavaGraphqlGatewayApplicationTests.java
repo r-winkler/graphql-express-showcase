74185ef73509cc4ff8e266513f06bb71994bc568
René Winkler <info@rewiso.ch>
2019-07-18 10:16:24 +0200

    java graphql gateway

---
+++ java-graphql-gateway/src/test/java/ch/rewiso/javagraphqlgateway/JavaGraphqlGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.rewiso.javagraphqlgateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaGraphqlGatewayApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
